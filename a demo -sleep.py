ceea3012c8016ec1aceabbb8bdb84b62cb0f66e5
davidsong <davidsong@sina.com>
2018-01-18 06:40:11 +0800

    修改间隔从3秒到两小时，

--- a demo -sleep.py
+++ a demo -sleep.py
@@ -1,5 +1,6 @@
 # import 是引入模块（其他.py文件）
 # time 模块里面有很多关于 时间的函数 引入以后可以利用里面的函数
+# 运行以后可以在自带控制台ctrl+c停止程序，在pycharm中则是ctrl+f2 （ctrl+f5运行程序）
 import webbrowser
 import time
 # ctime 就是 time模块里面的一个函数
@@ -13,6 +14,8 @@ total_break = 3
 break_count = 0
 
 while break_count < total_break:
-    time.sleep(3)
+    # 修改从3秒变为两小时 可以直接在参数里面加减乘除
+    # time.sleep(3)
+    time.sleep(3 * 60 * 60 * 2)
     webbrowser.open("www.youtube.com")
     break_count = break_count + 1
