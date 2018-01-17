777ef621b4f2efd94fec529591ed71e10949f498
davidsong <davidsong@sina.com>
2018-01-18 06:34:33 +0800

    解释import 解释ctime 添加print

--- a demo -sleep.py
+++ a demo -sleep.py
@@ -1,5 +1,9 @@
+# import 是引入模块（其他.py文件）
+# time 模块里面有很多关于 时间的函数 引入以后可以利用里面的函数
 import webbrowser
 import time
+# ctime 就是 time模块里面的一个函数
+print("this program starter on" + time.ctime())
 # 学会使用while
 # 具体用法：定义循环的 初始变量和最终变量
 #while 初始变量 < 最终变量 ：
