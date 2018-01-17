c87718650973687b91b8ee1288240482bd7b5042
davidsong <davidsong@sina.com>
2018-01-18 06:14:48 +0800

    等待五秒打开youtube v2 加注释

--- a demo -sleep.py
+++ a demo -sleep.py
@@ -1,5 +1,14 @@
 import webbrowser
 import time
+# 学会使用while
+# 具体用法：定义循环的 初始变量和最终变量
+#while 初始变量 < 最终变量 ：
+#        执行语句
+#初始变量 = 初始变量 + 1
+total_break = 3
+break_count = 0
 
-time.sleep(5)
-webbrowser.open("www.youtube.com")
+while break_count < total_break:
+    time.sleep(3)
+    webbrowser.open("www.youtube.com")
+    break_count = break_count + 1
